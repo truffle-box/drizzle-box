92092789d8c41b0b432f0e4645554df786303ff9
Unknown <joshquin@gmail.com>
2017-11-30 18:01:09 -0500

    Updated SimpleStorage form

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -11,20 +11,14 @@ class Home extends Component {
     this.handleInputChange = this.handleInputChange.bind(this)
 
     this.state = {
+      storageAmount: 0,
       tokenRecipientAddress: '',
       tokenTransferAmount: 0
     }
   }
 
-  componentDidMount() {
-    window.addEventListener('DrizzleReady', () => {
-      // `Children.only` enables us not to add a <div /> for nothing
-      return this.setState({ contractsReady: true })
-    })
-  }
-
   handleSetButton() {
-    this.contracts.SimpleStorage.methods.set(8).send()
+    this.contracts.SimpleStorage.methods.set(this.state.storageAmount).send()
   }
 
   handleSendTokens() {
@@ -36,47 +30,45 @@ class Home extends Component {
   }
 
   render() {
-    // Loading states
-    var storedData = 'Loading...'
-    var tokenSymbol = ''
-    var tokenSupply = 'Loading...'
-    var tokenBalance = 'Loading...'
+    // SimpleStorage Vars
+    var storedData = this.props.drizzleStatus.initialized ? this.contracts.SimpleStorage.methods.storedData.data() : 'Loading...'
 
-    if (this.props.drizzleStatus.initialized)
-    {
-      // SimpleStorage Vars
-      storedData = this.contracts.SimpleStorage.methods.storedData.data()
-
-      // TutorialToken Vars
-      tokenSymbol = this.contracts.TutorialToken.methods.symbol.data()
-      tokenSupply = this.contracts.TutorialToken.methods.totalSupply.data()
-      tokenBalance = this.contracts.TutorialToken.methods.balanceOf.data(this.props.accounts[0])
-    }
+    // TutorialToken Vars
+    var tokenSymbol = this.props.drizzleStatus.initialized ? this.contracts.TutorialToken.methods.symbol.data() : ''
+    var tokenSupply = this.props.drizzleStatus.initialized ? this.contracts.TutorialToken.methods.totalSupply.data() : 'Loading...'
+    var tokenBalance = this.props.drizzleStatus.initialized ? this.contracts.TutorialToken.methods.balanceOf.data(this.props.accounts[0]) : 'Loading...'
 
     return(
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1">
-            <h1>Drizzle Gym</h1>
-            <p>This is a React app that puts Drizzle through its paces.</p>
+            <h1>Drizzle Examples</h1>
+            <p>Here you'll find two example contract front-ends.</p>
+          </div>
+
+          <div className="pure-u-1-1">
+            <h2>SimpleStorage</h2>
+            <p><strong>Stored Value</strong>: {storedData}</p>
+            <form className="pure-form pure-form-stacked">
+              <input name="storageAmount" type="number" value={this.state.storageAmount} onChange={this.handleInputChange} />
+              <button className="pure-button" type="button" onClick={this.handleSetButton}>Store Value of {this.state.storageAmount}</button>
+            </form>
+
+            <br/>
+          </div>
+
+          <div className="pure-u-1-1">
+            <h2>TutorialToken</h2>
+            <p><strong>Total Supply</strong>: {tokenSupply} {tokenSymbol}</p>
+            <p><strong>My Balance</strong>: {tokenBalance}</p>
+            <h3>Send Tokens</h3>
+            <form className="pure-form pure-form-stacked">
+              <input name="tokenRecipientAddress" type="text" value={this.state.tokenRecipientAddress} onChange={this.handleInputChange} placeholder="Address" />
+              <input name="tokenTransferAmount" type="number" value={this.state.tokenTransferAmount} onChange={this.handleInputChange} placeholder="Amount" />
+              <button className="pure-button" type="button" onClick={this.handleSendTokens}>Send Tokens to {this.state.tokenRecipientAddress}</button>
+            </form>
           </div>
         </div>
-
-        <h2>SimpleStorage</h2>
-        <p><strong>Stored Value</strong>: {storedData}</p>
-        <button className="pure-button" type="button" onClick={this.handleSetButton}>Store Value of 8</button>
-
-        <br/><br/>
-
-        <h2>TutorialToken</h2>
-        <p><strong>Total Supply</strong>: {tokenSupply} {tokenSymbol}</p>
-        <p><strong>My Balance</strong>: {tokenBalance}</p>
-        <h3>Send Tokens</h3>
-        <form className="pure-form pure-form-stacked">
-          <input name="tokenRecipientAddress" type="text" value={this.state.tokenRecipientAddress} onChange={this.handleInputChange} placeholder="Address" />
-          <input name="tokenTransferAmount" type="number" value={this.state.tokenTransferAmount} onChange={this.handleInputChange} placeholder="Amount" />
-          <button className="pure-button" type="button" onClick={this.handleSendTokens}>Send Tokens to {this.state.tokenRecipientAddress}</button>
-        </form>
       </main>
     )
   }
