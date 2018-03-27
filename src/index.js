cb9366e74c2917737a78daf08bd563efa8f9563a
DiscRiskandBisque <joshquin@gmail.com>
2018-03-27 03:44:24 -0400

    Updated Drizzle libs to v1.1, mono-store

--- src/index.js
+++ src/index.js
@@ -1,56 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import { Provider } from 'react-redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { DrizzleProvider } from 'drizzle-react'
 
 // Layouts
 import App from './App'
 import HomeContainer from './layouts/home/HomeContainer'
-import LoadingContainer from './layouts/loading/LoadingContainer'
+import { LoadingContainer } from 'drizzle-react-components'
 
-// Contracts
-import ComplexStorage from './../build/contracts/ComplexStorage.json'
-import SimpleStorage from './../build/contracts/SimpleStorage.json'
-import TutorialToken from './../build/contracts/TutorialToken.json'
-
-// Redux Store
 import store from './store'
+import drizzleOptions from './drizzleOptions'
 
 // Initialize react-router-redux.
 const history = syncHistoryWithStore(browserHistory, store)
 
-// Set Drizzle options.
-const options = {
-  web3: {
-    block: false,
-    fallback: {
-      type: 'ws',
-      url: 'ws://127.0.0.1:8545'
-    }
-  },
-  contracts: [
-    ComplexStorage,
-    SimpleStorage,
-    TutorialToken
-  ],
-  events: {
-    SimpleStorage: ['StorageSet']
-  }
-}
-
 ReactDOM.render((
-    <DrizzleProvider options={options}>
-      <Provider store={store}>
-        <LoadingContainer>
-          <Router history={history}>
-            <Route path="/" component={App}>
-              <IndexRoute component={HomeContainer} />
-            </Route>
-          </Router>
-        </LoadingContainer>
-      </Provider>
+    <DrizzleProvider options={drizzleOptions} store={store}>
+      <LoadingContainer>
+        <Router history={history}>
+          <Route path="/" component={App}>
+            <IndexRoute component={HomeContainer} />
+          </Route>
+        </Router>
+      </LoadingContainer>
     </DrizzleProvider>
   ),
   document.getElementById('root')
--- src/store.js
+++ src/store.js
@@ -3,20 +3,33 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { routerMiddleware } from 'react-router-redux'
 import reducer from './reducer'
+import rootSaga from './rootSaga'
+import createSagaMiddleware from 'redux-saga'
+import { generateContractsInitialState } from 'drizzle'
+import drizzleOptions from './drizzleOptions'
 
 // Redux DevTools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const routingMiddleware = routerMiddleware(browserHistory)
+const sagaMiddleware = createSagaMiddleware()
+
+const initialState = {
+  contracts: generateContractsInitialState(drizzleOptions)
+}
 
 const store = createStore(
   reducer,
+  initialState,
   composeEnhancers(
     applyMiddleware(
       thunkMiddleware,
-      routingMiddleware
+      routingMiddleware,
+      sagaMiddleware
     )
   )
 )
+
+sagaMiddleware.run(rootSaga)
 
 export default store
