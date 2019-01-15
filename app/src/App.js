fcec6ffec718938751dc01cd5822af19902d63b3
Adrian Li <li.adrianmc@gmail.com>
2019-01-15 00:07:22 -0500

    add drizzle to react project

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { DrizzleProvider } from "drizzle-react";
+import { LoadingContainer } from "drizzle-react-components";
+
+import "./App.css";
+
+import drizzleOptions from "./drizzleOptions";
+import MyContainer from "./MyContainer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <DrizzleProvider options={drizzleOptions}>
+        <LoadingContainer>
+          <MyContainer />
+        </LoadingContainer>
+      </DrizzleProvider>
     );
   }
 }
