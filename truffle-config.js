2228877ffc43ddba59fc0ed5a626423827748a87
Cruz Molina <cruz.adam.molina@gmail.com>
2019-06-11 11:35:02 -0700

    Add MM-friendly develop network

--- truffle-config.js
+++ truffle-config.js
@@ -4,4 +4,9 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  networks: {
+    develop: {
+      port: 8545
+    }
+  }
 };
