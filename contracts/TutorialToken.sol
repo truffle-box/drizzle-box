26f9ef5a0c586f5c27fc5b78999d255a773270ac
MNG <17662533+xMNG@users.noreply.github.com>
2020-01-23 11:58:18 -0500

    updated openzeppelin and TutorialToken

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -1,12 +1,12 @@
 pragma solidity >=0.4.21 <0.7.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TutorialToken is ERC20 {
     string public name = "TutorialToken";
     string public symbol = "TT";
-    uint public decimals = 2;
-    uint public INITIAL_SUPPLY = 12000;
+    uint256 public decimals = 2;
+    uint256 public INITIAL_SUPPLY = 12000;
 
     constructor() public {
         _mint(msg.sender, INITIAL_SUPPLY);
