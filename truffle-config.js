f1046c6a4ba3fab3bff887b2adbbd08d2ea3db74
MNG <17662533+xMNG@users.noreply.github.com>
2020-02-26 10:34:32 -0500

    allow migration to port 8545

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,10 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
     }
   }
 };
