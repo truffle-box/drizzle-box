a137ea1c0f646ebb82c299689c9375418ead6dea
MNG <17662533+xMNG@users.noreply.github.com>
2020-02-26 10:22:41 -0500

    adjusted to fit 2 spaces styling convention

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,7 @@
-import React from "react";
 import { DrizzleContext } from "@drizzle/react-plugin";
 import { Drizzle, generateStore } from "@drizzle/store";
-
+import React from "react";
 import "./App.css";
-
 import drizzleOptions from "./drizzleOptions";
 import MyComponent from "./MyComponent";
 
@@ -11,23 +9,23 @@ const drizzleStore = generateStore(drizzleOptions);
 const drizzle = new Drizzle(drizzleOptions, drizzleStore)
 
 const App = () => {
-    return (
-      <DrizzleContext.Provider drizzle={drizzle}>
-        <DrizzleContext.Consumer>
-          {drizzleContext => {
-            const {drizzle, drizzleState, initialized} = drizzleContext;
-            
-            if(!initialized) {
-              return "Loading..."
-            }
-            
-            return (
-              <MyComponent drizzle={drizzle} drizzleState={drizzleState} />
-              )
-            }}
-        </DrizzleContext.Consumer>
-      </DrizzleContext.Provider>
-    );
-  }
+  return (
+    <DrizzleContext.Provider drizzle={drizzle}>
+      <DrizzleContext.Consumer>
+        {drizzleContext => {
+          const { drizzle, drizzleState, initialized } = drizzleContext;
+
+          if (!initialized) {
+            return "Loading..."
+          }
+
+          return (
+            <MyComponent drizzle={drizzle} drizzleState={drizzleState} />
+          )
+        }}
+      </DrizzleContext.Consumer>
+    </DrizzleContext.Provider>
+  );
+}
 
 export default App;
